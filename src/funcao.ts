c1335fd162b0071ebaa8efcd3cecca5a10930822
Leticia Dias <leticia04dias@gmail.com>
2024-08-13 08:23:37 -0300

    first commit

---
+++ src/funcao.ts
@@ -0,0 +1,13 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function saudar(name: string): string {
+    return "Olá " + name;
+}
+
+const resultado = multiplicar(8, 4);
+console.log(resultado);
+
+const saudacao = saudar('Letícia');
+console.log(saudacao);
